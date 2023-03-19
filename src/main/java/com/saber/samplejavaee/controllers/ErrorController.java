22241f9106c4c0252be9c7c993962c2d0b5c436e
Saber Azizi <azizi.s@tiddev.com>
2023-03-19 08:30:09 +0330

    impl person

--- src/main/java/com/saber/samplejavaee/controllers/ErrorController.java
+++ src/main/java/com/saber/samplejavaee/controllers/ErrorController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/error",name = "errorController",displayName = "errorController")
 public class ErrorController extends HttpServlet {
